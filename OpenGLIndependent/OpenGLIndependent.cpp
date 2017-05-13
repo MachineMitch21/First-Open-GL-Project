1b8de1594d8df65ae79744358c1de375f041b11b
MachineMitch21 <mitchschutt@gmail.com>
2017-05-12 20:21:26 -0400

    Cleaned up main a bit and experimented with different methods of generating and deleting meshes during runtime

--- OpenGLIndependent/OpenGLIndependent.cpp
+++ OpenGLIndependent/OpenGLIndependent.cpp
@@ -13,133 +13,55 @@ int main()
 {
 	Window window("Antisocial Engine", 800, 600);
 
-		GLfloat vert1 [] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert2 [] = { 0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,	};
-		GLfloat vert3 [] = { 0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  };
-		GLfloat vert4 [] = { 0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  };
-		GLfloat vert5 [] = {-0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert6 [] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert7 [] = {-0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert8 [] = { 0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  };
-		GLfloat vert9 [] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,	};
-		GLfloat vert10[] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,	};
-		GLfloat vert11[] = {-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,  };
-		GLfloat vert12[] = {-0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert13[] = {-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  };
-		GLfloat vert14[] = {-0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,	};
-		GLfloat vert15[] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert16[] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert17[] = {-0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert18[] = {-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  };
-		GLfloat vert19[] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,	};
-		GLfloat vert20[] = { 0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  };
-		GLfloat vert21[] = { 0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert22[] = { 0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert23[] = { 0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,  };
-		GLfloat vert24[] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,	};
-		GLfloat vert25[] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert26[] = { 0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,	};
-		GLfloat vert27[] = { 0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  };
-		GLfloat vert28[] = { 0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  };
-		GLfloat vert29[] = {-0.5f,-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,	};
-		GLfloat vert30[] = {-0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert31[] = {-0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,	};
-		GLfloat vert32[] = { 0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,  };
-		GLfloat vert33[] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,	};
-		GLfloat vert34[] = { 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,	};
-		GLfloat vert35[] = {-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,  };
-		GLfloat vert36[] = {-0.5f, 0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f	};
-
-		GLfloat vertices[] = { -0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-								0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-								-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-								-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f	};
+	GLfloat vertices[] = { -0.5f,-0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+							0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+							-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+							-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f	};
 
 
 
-		glm::vec3 cubePositions[] = {
-			glm::vec3(1.0f, -0.25f, 0.0f),
-			glm::vec3(2.0f, 5.0f, -15.0f),
-			glm::vec3(-1.5f, -2.2f, -2.5f),
-			glm::vec3(-2.5f, 1.0f, -5.5f),
-			glm::vec3(0.0f, 0.0f, -5.0f)
-		};
-
-
-
-		/*Vertex vertices[] = {	Vertex(vert1 , 8),
-								Vertex(vert2 , 8),
-								Vertex(vert3 , 8),
-								Vertex(vert4 , 8),
-								Vertex(vert5 , 8),
-								Vertex(vert6 , 8),
-								Vertex(vert7 , 8),
-								Vertex(vert8 , 8),
-								Vertex(vert9 , 8),
-								Vertex(vert10, 8),
-								Vertex(vert11, 8),
-								Vertex(vert12, 8),
-								Vertex(vert13, 8),
-								Vertex(vert14, 8),
-								Vertex(vert15, 8),
-								Vertex(vert16, 8),
-								Vertex(vert17, 8),
-								Vertex(vert18, 8),
-								Vertex(vert19, 8),
-								Vertex(vert20, 8),
-								Vertex(vert21, 8),
-								Vertex(vert22, 8),
-								Vertex(vert23, 8),
-								Vertex(vert24, 8),
-								Vertex(vert25, 8),
-								Vertex(vert26, 8),
-								Vertex(vert27, 8),
-								Vertex(vert28, 8),
-								Vertex(vert29, 8),
-								Vertex(vert30, 8),
-								Vertex(vert31, 8),
-								Vertex(vert32, 8),
-								Vertex(vert33, 8),
-								Vertex(vert34, 8),
-								Vertex(vert35, 8),
-								Vertex(vert36, 8) };*/
+	glm::vec3 cubePositions[] = {
+		glm::vec3(1.0f, -0.25f, 0.0f),
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-2.5f, 1.0f, -5.5f),
+		glm::vec3(0.0f, 0.0f, -5.0f),
+		glm::vec3(-1.0f, 1.5f, 0.0f),
+		glm::vec3(-3.0f, 1.5f, -12.5f)
+	};
 		
-		
-		
-
-
 	GLuint indices[] = {
 		0, 1, 3,
 		1, 2, 3
@@ -150,18 +72,18 @@ int main()
 
 	Shader shader(files, types);
 
-	Mesh cube1(vertices, 36, 8);
+	/*Mesh cube1(vertices, 36, 8);
 	Mesh cube2(vertices, 36, 8);
 	Mesh cube3(vertices, 36, 8);
 	Mesh cube4(vertices, 36, 8);
-	Mesh cube5(vertices, 36, 8);
+	Mesh cube5(vertices, 36, 8);*/
 
-	std::vector<Mesh> meshes;
-	meshes.push_back(cube1);
+	std::vector<Mesh*> meshes;
+	/*meshes.push_back(cube1);
 	meshes.push_back(cube2);
 	meshes.push_back(cube3);
 	meshes.push_back(cube4);
-	meshes.push_back(cube5);
+	meshes.push_back(cube5);*/
 
 	Texture texture("res\\bricks.jpg");
 
@@ -174,14 +96,23 @@ int main()
 	while (!window.IsClosed()) {
 		window.clear(0.2f, 0.3f, 0.3f, 1.0f);
 
+		if (window.isKeyPressed(GLFW_KEY_M)) {
+			meshes.push_back(new Mesh(vertices, 36, 8));
+		}
+
+		if (window.isKeyPressed(GLFW_KEY_D)) {
+			if(meshes.size() > 0)
+				meshes.pop_back();
+		}
+
 		if (window.isKeyPressed(GLFW_KEY_W) && window.isKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
 			for(int i = 0; i < meshes.size(); i++)
-				meshes[i].setDrawMode(GL_LINE);
+				meshes[i]->setDrawMode(GL_LINE);
 		}
 
 		if (window.isKeyPressed(GLFW_KEY_S) && window.isKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
 			for (int i = 0; i < meshes.size(); i++)
-				meshes[i].setDrawMode(GL_FILL);
+				meshes[i]->setDrawMode(GL_FILL);
 		}
 
 		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_1)) {
@@ -193,19 +124,6 @@ int main()
 		}
 
 		shader.bind();
-
-		/*// Create transformations
-		glm::mat4 transform;
-		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-		transform = glm::rotate(transform, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
-		std::cout << (GLfloat)glfwGetTime() << std::endl;
-
-		// Get matrix's uniform location and set matrix
-		GLint transformLoc = glGetUniformLocation(shader.getProgram(), "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));*/
-
-		// Create transformations
 		
 		glm::mat4 view;
 		glm::mat4 projection;
@@ -223,7 +141,7 @@ int main()
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		for (GLuint i = 0; i < meshes.size(); i++) {
-			meshes[i].draw(cubePositions[i], shader.getProgram());
+			meshes[i]->draw(cubePositions[i], shader.getProgram());
 		}
 
 		window.update();
